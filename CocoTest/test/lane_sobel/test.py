46a5ce1f48b2f3fd246e1b65df31ebf40fc1c917
jimmy <jimmy@satellogic.com>
2022-05-31 22:03:46 -0300

    fix comparison and kernel creation

--- CocoTest/test/lane_sobel/test.py
+++ CocoTest/test/lane_sobel/test.py
@@ -1,10 +1,11 @@
-import cocotb
+from cocotb import fork
 from cocotb.clock import Clock
 from cocotb.triggers import Timer, RisingEdge
 from cocotb.regression import TestFactory as TF
 from os import environ
 from PIL import Image
 import numpy as np
+from cv2 import getDerivKernels
 
 HOME = environ['HOME']
 REPO_PATH = HOME + '/Documents/Vision-Course'
@@ -14,8 +15,8 @@ OUT_PATH = REPO_PATH + '/CocoTest/test/lane_sobel/'
 CLK_PERIOD = 10  # ns
 SQ_LIMIT = 14*2
 SQ_SUM_FACTOR = 13
-SQ_SUM_LIMIT = 13
-MAX_SUM_LIMIT = (1 << SQ_SUM_LIMIT) - 1
+SQ_SUM_LIMIT = 18
+MAX_SUM_LIMIT = (1 << SQ_SUM_FACTOR) - 1
 
 
 async def Reset(dut):
@@ -85,52 +86,113 @@ def convolve(image, kernel):
     return output
 
 
-def get_target(image, kernel_x, kernel_y):
+def get_target(image, kernel):
+    kernel_y = kernel
+    kernel_x = np.transpose(kernel_y)
     g_x_2 = convolve(image, kernel_x)
     g_y_2 = convolve(image, kernel_y)
     g_sum_2 = (g_x_2 + g_y_2) >> SQ_SUM_FACTOR
-    g_sum_2[np.where(g_sum_2 > MAX_SUM_LIMIT)] = MAX_SUM_LIMIT
-    g2_limit = np.uint16(g_sum_2 % (1 << SQ_SUM_FACTOR))
+    g2_limit = np.zeros(g_sum_2.shape, dtype=np.uint32)
+    g2_limit[:, :] = g_sum_2[:, :]
+    g2_limit[np.where(g2_limit > MAX_SUM_LIMIT)] = MAX_SUM_LIMIT
+    g2_limit = g2_limit % (1 << SQ_SUM_LIMIT)
     # The table implemented in the HDL makes the
     # 255-8*g2_limit calculation.
     lum_new = 255-np.uint8(np.sqrt(8*g2_limit))
     output = np.zeros(image.shape, dtype=np.uint8)
     for i in range(3):
         output[:, :, i] = lum_new
+    return (output, g2_limit, g_sum_2, g_x_2, g_y_2)
+
+
+async def collect_data(clk, signal, height, width,
+                       dtype=np.uint32):
+    mask = (1 << len(signal)) - 1
+    output = np.zeros((height, width), dtype=dtype)
+    for i in range(height):
+        for j in range(width):
+            output[i, j] = signal.value & mask
+            await RisingEdge(clk)
     return output
 
 
+def validate_matrix(kernel_width, a, b, LINE_DELAY=1):
+    # This validation take account of the dirty margins
+    assert a.shape == b.shape
+    offset = kernel_width//2
+    if len(a.shape) == 2:
+        a1 = a[offset+LINE_DELAY:, offset+LINE_DELAY:]
+        b1 = b[offset:-LINE_DELAY, offset:-LINE_DELAY]
+    elif len(a.shape) == 3:
+        a1 = a[offset+LINE_DELAY:, offset+LINE_DELAY:, :]
+        b1 = b[offset:-LINE_DELAY, offset:-LINE_DELAY, :]
+    assert (a1 == b1).all()
+
+
+def get_kernel(filter):
+    assert filter in ['sobel', ]
+    if filter == 'sobel':
+        sobel3x = getDerivKernels(1, 0,  3)
+        kernel = np.int8(np.outer(sobel3x[0], sobel3x[1]))
+    return kernel
+
+
 async def test_filter(dut, filter_name, img_name):
-    cocotb.fork(Clock(dut.clk, period=CLK_PERIOD, units='ns').start())
+    fork(Clock(dut.clk, period=CLK_PERIOD, units='ns').start())
     await Reset(dut)
 
-    CORE_LATENCY = 10
+    CORE_LATENCY = 7
 
     test_image = IMG_PATH + img_name
     image = np.array(Image.open(test_image), dtype=np.uint8)
     height, width, _ = image.shape
 
-    kernel_x = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], np.int8)
-    kernel_y = np.transpose(kernel_x)
-    target = get_target(image, kernel_x, kernel_y)
+    kernel = get_kernel(filter_name)
+    output_target, g2_limit_target, \
+    g_sum_2_target, g_x_2_target, \
+    g_y_2_target = get_target(image, kernel)
 
-    im = Image.fromarray(target)
+    im = Image.fromarray(output_target)
     im.save(OUT_PATH + img_name.replace('.bmp', f'_{filter_name}_target.bmp'))
 
     flat_image = np.array(b8_2_b24(np.uint32(image)).flatten())
 
-    cocotb.fork(send_image(dut, flat_image))
+    fork(send_image(dut, flat_image))
 
     await RisingEdge(dut.de_in)
 
-    for _ in range(CORE_LATENCY):
+    for _ in range(CORE_LATENCY-3):
         await RisingEdge(dut.clk)
 
+    g_x_2_fork = fork(collect_data(clk=dut.clk,
+                                   signal=dut.g_x_2,
+                                   height=height,
+                                   width=width))
+
+    g_y_2_fork = fork(collect_data(clk=dut.clk,
+                                   signal=dut.g_y_2,
+                                   height=height,
+                                   width=width))
+    await RisingEdge(dut.clk)
+
+    g_sum_2_fork = fork(collect_data(clk=dut.clk,
+                                     signal=dut.g_sum_2,
+                                     height=height,
+                                     width=width))
+    await RisingEdge(dut.clk)
+
+    g2_limit_fork = fork(collect_data(clk=dut.clk,
+                                      signal=dut.g2_limit,
+                                      height=height,
+                                      width=width))
+    await RisingEdge(dut.clk)
+
+    output = np.zeros(image.shape, dtype=np.uint8)
     for i in range(height):
         for j in range(width):
             data_out = dut.data_out.value
             for k in range(3):
-                image[i, j, k] = (data_out >> ((2-k)*8)) & 0xFF
+                output[i, j, k] = (data_out >> ((2-k)*8)) & 0xFF
             await RisingEdge(dut.clk)
 
     await RisingEdge(dut.clk)
@@ -139,27 +201,24 @@ async def test_filter(dut, filter_name, img_name):
 
     await RisingEdge(dut.clk)
 
-    result_r = image[2:, :-3, 2]
-    target_r = target[2:, 2:-1, 2]
+    # Debug of different stages
+    g_x_2 = g_x_2_fork.retval
+    g_y_2 = g_y_2_fork.retval
+    g_sum_2 = g_sum_2_fork.retval
+    g2_limit = g2_limit_fork.retval
+    validate_matrix(kernel.shape[0], g_x_2, g_x_2_target)
+    validate_matrix(kernel.shape[0], g_y_2, g_y_2_target)
+    validate_matrix(kernel.shape[0], g_sum_2, g_sum_2_target)
+    validate_matrix(kernel.shape[0], g2_limit, g2_limit_target)
 
-    assert result_r.shape == target_r.shape
-    errors = 0
-    for row in range(result_r.shape[0]):
-        for col in range(target_r.shape[1]):
-            val = result_r[row, col]
-            exp = target_r[row, col]
-            # msg = f'row: {row}, col: {col}, val: {val}, exp: {exp}'
-            # assert val == exp, msg
-            if val != exp:
-                errors += 1
-    print('errors', errors)
+    # Compare final results
+    validate_matrix(kernel.shape[0], output, output_target)
 
-    im = Image.fromarray(image)
+    im = Image.fromarray(output)
     im.save(OUT_PATH + img_name.replace('.bmp', f'_{filter_name}_out.bmp'))
 
 
 tf_sobel = TF(test_filter)
 tf_sobel.add_option('filter_name', ['sobel'])
-# tf_sobel.add_option('img_name', [f'street_{n}.bmp' for n in ['A', 'B', 'C']])
-tf_sobel.add_option('img_name', [f'street_{n}.bmp' for n in ['A']])
+tf_sobel.add_option('img_name', [f'street_{n}.bmp' for n in ['A', 'B', 'C']])
 tf_sobel.generate_tests()
--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ```
 sudo apt install ghdl gtkwave
-pip3 install cocotb pillow numpy
+pip3 install cocotb pillow numpy opencv-python
 ```
 
 ## Using sources from FPGA Vision Course
@@ -33,12 +33,12 @@ Or with pipeline cleaning
 rm -r results.xml sim_build/ __pycache__/; time make
 ```
 
-This command will test the 3 images in `FPGA-Vision/Test_Images` 
-and will post the output images in `CocoTest/test`.
+This command will test the 3 images in `FPGA-Vision/Test_Images`
+and will post the output images in `CocoTest/test`. It takes around
+of 3 minutes by image.
 
 ## Watch the Waveforms
 
 ```
 gtkwave waveform_sobel.vcd
 ```
-
